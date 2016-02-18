04aa62d46cd542f31fc24df19b97a5ff3996db28
melvynqwek <xtrmepower@gmail.com>
2016-02-18 11:24:23 +0800

    finished task 1

--- shell.c
+++ shell.c
@@ -3,7 +3,19 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define MAX_INPUT_LENGTH 80
+#define MAX_INPUT_LENGTH 200
+#define MAX_PATH_LENGTH 2000
+
+int compareLastChar(char* target, char* c) {
+
+	int lastCharPos = strlen(target) - 1;
+
+	if (target[lastCharPos] == *c) {
+		return lastCharPos;
+	}
+
+	return -1;
+}
 
 // To be polished
 void replaceChar(char* toReplace, char oldChar, char newChar) {
@@ -24,20 +36,75 @@ void getInput(char* input) {
 	fgets(input, MAX_INPUT_LENGTH, stdin);
 }
 
-int tokenize(char* output[], char* input, char delimiter) {
-	char *pch;
+int tokenize(char* output[], char* input, char *delimiter) {
+	char *pch = NULL;
+	char *rest = input;
+	int i = 0;
+
+
+	printf ("Input: %s Delimiter %s \n",input, delimiter);
+	while ((pch = strtok_r(rest, delimiter, &rest)))
+	{
+		printf ("%i %s \n",i,pch);
+		output[i] = pch;
+		i++;
+	}
+
+	return i;
+}
+
+
+int tokenize_old(char* output[], char* input, char delimiter) {
+	char *pch = NULL;
 	int i = 0;
 
 	pch = strtok (input, &delimiter);
 	while (pch != NULL)
 	{
+		printf ("%i %s \n",i,pch);
 		output[i] = pch;
-		//printf ("%i %s\n",i,pch);
 		pch = strtok (NULL, &delimiter);
 		i++;
 	}
 
 	return i;
+}
+
+void runChildProcess(char** paramList) {
+	char **pathLocationList;
+	pathLocationList = malloc(MAX_PATH_LENGTH);
+
+	char *pathVar = getenv("PATH");
+	printf("%s\n", pathVar);
+	int numOfPathLocations = tokenize(pathLocationList, pathVar, ":");
+
+	printf("numOfPathLocations = %i \n", numOfPathLocations);
+
+	char cmd[MAX_INPUT_LENGTH];
+	int lastCharPos = 0;
+	//char temp[MAX_INPUT_LENGTH];
+	for (int i = 0; i < numOfPathLocations; i++) {
+		//strcpy(cmd, "/bin/");
+		strcpy(cmd, pathLocationList[i]);
+		// check if last character is '/'
+		lastCharPos = compareLastChar(cmd, "/");
+		//printf("lastCharPos = %i\n", lastCharPos);
+
+		if (lastCharPos == -1) {
+			// append
+			//printf("call\n");
+			strcat(cmd, "/");
+		}
+		
+		strcat(cmd, paramList[0]);
+		//strcat(pathLocationList[i], "/");
+		printf("%i:)-> %s\n",i, cmd);
+		
+		//strcpy(cmd, pathLocationList[i]);
+		//strcat(cmd, paramList[0]);
+		execvp(cmd, paramList);
+	}
+	free(pathLocationList);
 }
 
 int main(int argc, char *argv[], char *envp[]) {
@@ -46,14 +113,18 @@ int main(int argc, char *argv[], char *envp[]) {
 	while (1) {
 		wait(NULL);
 		char input[MAX_INPUT_LENGTH];
-		printf("my pr %d", getpid());
+		//printf("my pr %d", getpid());
 		getInput(input);
-		replaceChar(input, '\n', '\0');
+		//replaceChar(input, '\n', '\0');
+		int temp = compareLastChar(input, "\n");
+		if (temp != -1) {
+			input[temp] = '\0';
+		}
 
 		char **paramList;
 		paramList = malloc(MAX_INPUT_LENGTH);
 
-		int numOfParam = tokenize(paramList, input, ' ');
+		int numOfParam = tokenize(paramList, input, " ");
 		realloc(paramList, numOfParam+1);
 		paramList[numOfParam] = NULL;
 
@@ -63,14 +134,7 @@ int main(int argc, char *argv[], char *envp[]) {
 		}
 		else if (pid == 0) {
 			// in child
-			char cmd[MAX_INPUT_LENGTH];
-			strcpy(cmd, "/bin/");
-			strcat(cmd, paramList[0]);
-			//printf("cmd = %s", paramList[1]);
-
-			//execvp(cmd, paramList);
-			execvp(cmd, paramList);
-
+			runChildProcess(paramList);
 			exit(0);
 		} else {
 			perror("fork error");
@@ -78,6 +142,5 @@ int main(int argc, char *argv[], char *envp[]) {
 		}
 		free(paramList);
 	}
-	
 	exit(0);
 }
