1cebf78153a283dba2b34a3d29ff4e675b7c29c3
melvynqwek <xtrmepower@gmail.com>
2016-02-19 16:12:38 +0800

    searched for SHELL_PATH

--- prog.c
+++ prog.c
@@ -17,11 +17,23 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define SHELL_PATH_STRING "SHELL_PATH"
+
 int main(int argc, char **argv, char** envp) {
-	
+
+	char* shellPath;
+
 	for (char** env = envp; *env != NULL; env++) {
 		char* thisEnv = *env;
 		printf("%s\n", thisEnv);
+
+		shellPath = strstr(thisEnv, SHELL_PATH_STRING);
+	}
+
+	if (shellPath == NULL) {
+		printf("%s value is UNKNOWN.\n", SHELL_PATH_STRING);
+	} else {
+		printf("%s value is %s\n", SHELL_PATH_STRING, shellPath);
 	}
 
 	return 0;
