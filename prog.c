beef610308c49a87178f252a225ba9f689b48ac4
melvynqwek <xtrmepower@gmail.com>
2016-02-19 13:30:27 +0800

    Prints out all environment variables to UNIX shell.

--- prog.c
+++ prog.c
@@ -1,15 +1,28 @@
+/*
+	prog.c
+
+	CS2106 Introduction to Operating Systems
+	Assignment 1
+
+	Written by:
+	Name: 	Qwek Siew Weng, Melvyn
+	Matric:	A0111821X
+
+	Name:	Jay Chua Soon Yao
+	Matric:	A0111784H
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv, char** envp)
-{
-	char** env;
-	for (env = envp; *env != 0; env++)
-	{
+int main(int argc, char **argv, char** envp) {
+	
+	for (char** env = envp; *env != NULL; env++) {
 		char* thisEnv = *env;
-		printf("%s\n", thisEnv);    
+		printf("%s\n", thisEnv);
 	}
+
 	return 0;
 }
