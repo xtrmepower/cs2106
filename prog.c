f18069131eeeb5dc270a91c7926fced23bc033f8
melvynqwek <xtrmepower@gmail.com>
2016-02-23 21:49:53 +0800

    added comments to prog.c

--- prog.c
+++ prog.c
@@ -23,13 +23,16 @@ int main(int argc, char **argv, char** envp) {
 
 	char* shellPath;
 
+	// Print the list of environment variables that were passed in.
 	for (char** env = envp; *env != NULL; env++) {
 		char* thisEnv = *env;
 		printf("%s\n", thisEnv);
 
+		// Check to see if the SHELL_PATH string is in the current line.
 		shellPath = strstr(thisEnv, SHELL_PATH_STRING);
 	}
 
+	// Print the shell path if it is found.
 	if (shellPath == NULL) {
 		printf("%s value is UNKNOWN.\n", SHELL_PATH_STRING);
 	} else {
